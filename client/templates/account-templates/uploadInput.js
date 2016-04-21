19881d5de596ac74981b3067f2e23a82b558e3fd
Rogerio J <setaware@gmail.com>
2016-04-21 10:58:26 -0430

    Style modifications.

--- client/templates/account-templates/uploadInput.js
+++ client/templates/account-templates/uploadInput.js
@@ -13,25 +13,31 @@ Template.uploadInput.events({
     var imageObject = ProfilePic.insert(image);
     // The image id is stored in the image object
     var imageId = imageObject._id;
+	// The image URL constructed from the absolute URL of the project
     var imageUrl = Meteor.absoluteUrl() +"cfs/files/profilePic/"+imageId;
+	// The file name
+	var imageName = imageObject.original.name;
 	//Store the new picture ID and url in a session variable
 	Session.set('signUpPicID', imageUrl);
 	//Check by console
     Meteor.setTimeout(function () {
         console.log("Url " + imageUrl);
       }, 3 * 1000);
-
-	document.getElementById("hiddenImage").style="display:block;"; //The image appears
+   
+	document.getElementById("hiddenName").style="display:block;"; //Uploading...
 	document.getElementById("uploadInput").style="display:none;"; //The file input button disappears
-	document.getElementsByClassName("image")[0].src = Meteor.absoluteUrl()+"img/loading_bar.gif";//Loading
-    // Create a reactive var to be used when the course is added
+
+	//function that check if the url of the image is available
 	var checker = setInterval(function(){
 		$.get(Session.get('signUpPicID')).done(function () {
-         document.getElementsByClassName("image")[0].src = Session.get('signUpPicID');	//Update the image prev
+		 //when the url are "done", the template reacts to this HTML code:
+         document.getElementById("hiddenName").innerHTML='<div class="form-group fileUpload btn btn-default btn-block btn-icon btn-file-upload">'+imageName+'<span><i class="fa fa-pencil-square-o"></i></span><input type="file" class="upload js-Profile" placeholder="Upload a Picture"/></div>';
 		 clearInterval(checker);
+		 //Bert alert as required
+		 Bert.alert('File uploaded');
          }).fail(function () {
         })
-	},3000);
+	},3000); //Interval of 3 seconds to check the url status, (for slow conections this might take between 3-12 secs)
 	
     if (imageId) {
       profilePicEdit = new ReactiveVar(imageId);
