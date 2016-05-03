5186e81a4dfd75e009be5df6116939234c4d8f50
Rogerio <rs7.contractors@gmail.com>
2016-05-03 11:31:58 -0400

    Unsubscribed link v2

--- client/templates/members/unSubscribe.js
+++ client/templates/members/unSubscribe.js
@@ -1,5 +1,15 @@
 Template.unSubscribe.onRendered(function() {
-  var email = window.location.search.split('=').pop();
-  var id_ = subscribers.findOne({email: email})._id;
-  subscribers.remove({_id: id_});
+   console.log('unsubscribing');
+});
+
+Template.unSubscribe.helpers({
+  unsubscribe: function(){
+  Meteor.setTimeout(function(){
+	  var email = window.location.search.split('=').pop();
+      var id_ = subscribers.findOne({email: email})._id;
+      subscribers.remove({_id: id_}); 
+	  console.log('Unsubscribed');
+  },3500);
+  return false;
+  }
 });
