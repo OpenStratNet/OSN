c82720d30f924e4d6b91ccb6ebd35dee8cf1668e
eugenkeller <eugenkeller.com@gmail.com>
2016-05-04 16:41:18 +0200

    Reformat and remove semicolon after if-statement

--- client/helpers/deleteHelpers.js
+++ client/helpers/deleteHelpers.js
@@ -1,33 +1,32 @@
 Template.applicationLayout.events({
   'click .jsNewsDelImage': function (evt, temp) {
     var deleteConfirmation = confirm('Really delete this entry?');
-    
     if (deleteConfirmation) {
       NewsEvents.update({_id: this._id}, {$unset: {coverImageId: ""}});
       Images.remove({_id: this.coverImageId});
       imageIdVarEdit.set(false);
-    };    
+    }
   },
   'click .jsNewsDelAttachment': function (evt, temp) {
     var deleteConfirmation = confirm('Really delete this entry?');
     if (deleteConfirmation) {
-     NewsEvents.update({_id: this._id}, {$unset: {attachmentId: ""}});
-     Attachments.remove({_id: this.attachmentId});
-     attachmentIdVarEdit.set(false);
-   };
+      NewsEvents.update({_id: this._id}, {$unset: {attachmentId: ""}});
+      Attachments.remove({_id: this.attachmentId});
+      attachmentIdVarEdit.set(false);
+    }
   },
   'click .jsPubsDelAttachment': function (evt, temp) {
     var deleteConfirmation = confirm('Really delete this entry?');
     if (deleteConfirmation) {
-     Publications.update({_id: this._id}, {$unset: {attachmentId: ""}});
-     Attachments.remove({_id: this.attachmentId});
-     attachmentIdVarEdit.set(false);
-   };
+      Publications.update({_id: this._id}, {$unset: {attachmentId: ""}});
+      Attachments.remove({_id: this.attachmentId});
+      attachmentIdVarEdit.set(false);
+    }
   },
   'click .jsMembersDelUser': function (evt, temp) {
     var deleteConfirmation = confirm('Really delete this member?');
     if (deleteConfirmation) {
-     Meteor.users.remove({_id: this._id});
-   };
+      Meteor.users.remove({_id: this._id});
+    }
   }
 });
