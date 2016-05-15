aa4fbca67f873c5b70a94f49a53e02f182325fa9
eugenkeller <eugenkeller.com@gmail.com>
2016-05-15 22:35:55 +0200

    Manage collection permissions via roles in settings.json

--- server/lib/collections.js
+++ server/lib/collections.js
@@ -2,17 +2,29 @@
 Meteor.methods({
   'Publications.insert': function (doc) {
     if (this.userId) {
-      Publications.insert(doc);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Publications.insert)) {
+        Publications.insert(doc);
+      } else {
+        console.log('Publications.insert: access denied.');
+      }
     }
   },
   'Publications.remove': function (id) {
     if (this.userId) {
-      Publications.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Publications.remove)) {
+        Publications.remove(id);
+      } else {
+        console.log('Publications.remove: access denied.');
+      }
     }
   },
   'Publications.update': function (selector, modifier) {
     if (this.userId) {
-      Publications.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Publications.update)) {
+        Publications.update(selector, modifier);
+      } else {
+        console.log('Publications.update: access denied.');
+      }
     }
   }
 });
@@ -21,17 +33,29 @@ Meteor.methods({
 Meteor.methods({
   'NewsEvents.insert': function (doc) {
     if (this.userId) {
-      NewsEvents.insert(doc);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.NewsEvents.insert)) {
+        NewsEvents.insert(doc);
+      } else {
+        console.log('NewsEvents.insert: access denied.');
+      }
     }
   },
   'NewsEvents.remove': function (id) {
     if (this.userId) {
-      NewsEvents.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.NewsEvents.remove)) {
+        NewsEvents.remove(id);
+      } else {
+        console.log('NewsEvents.remove: access denied.');
+      }
     }
   },
   'NewsEvents.update': function (selector, modifier) {
     if (this.userId) {
-      NewsEvents.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.NewsEvents.update)) {
+        NewsEvents.update(selector, modifier);
+      } else {
+        console.log('NewsEvents.update: access denied.');
+      }
     }
   }
 });
@@ -40,17 +64,29 @@ Meteor.methods({
 Meteor.methods({
   'Tags.insert': function (doc) {
     if (this.userId) {
-      Tags.insert(doc);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Tags.insert)) {
+        Tags.insert(doc);
+      } else {
+        console.log('Tags.insert: access denied.');
+      }
     }
   },
   'Tags.remove': function (id) {
     if (this.userId) {
-      Tags.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Tags.remove)) {
+        Tags.remove(id);
+      } else {
+        console.log('Tags.remove: access denied.');
+      }
     }
   },
   'Tags.update': function (selector, modifier) {
     if (this.userId) {
-      Tags.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Tags.update)) {
+        Tags.update(selector, modifier);
+      } else {
+        console.log('Tags.update: access denied.');
+      }
     }
   }
 });
@@ -59,17 +95,29 @@ Meteor.methods({
 Meteor.methods({
   'Categories.insert': function (doc) {
     if (this.userId) {
-      Categories.insert(doc);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Categories.insert)) {
+        Categories.insert(doc);
+      } else {
+        console.log('Categories.insert: access denied.');
+      }
     }
   },
   'Categories.remove': function (id) {
     if (this.userId) {
-      Categories.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Categories.remove)) {
+        Categories.remove(id);
+      } else {
+        console.log('Categories.remove: access denied.');
+      }
     }
   },
   'Categories.update': function (selector, modifier) {
     if (this.userId) {
-      Categories.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Categories.update)) {
+        Categories.update(selector, modifier);
+      } else {
+        console.log('Categories.update: access denied.');
+      }
     }
   }
 });
@@ -78,12 +126,20 @@ Meteor.methods({
 Meteor.methods({
   'Images.remove': function (id) {
     if (this.userId) {
-      Images.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Images.remove)) {
+        Images.remove(id);
+      } else {
+        console.log('Images.remove: access denied.');
+      }
     }
   },
   'Images.update': function (selector, modifier) {
     if (this.userId) {
-      Images.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Images.update)) {
+        Images.update(selector, modifier);
+      } else {
+        console.log('Images.update: access denied.');
+      }
     }
   }
 });
@@ -92,12 +148,20 @@ Meteor.methods({
 Meteor.methods({
   'ProfilePic.remove': function (id) {
     if (this.userId) {
-      ProfilePic.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.ProfilePic.remove)) {
+        ProfilePic.remove(id);
+      } else {
+        console.log('ProfilePic.remove: access denied.');
+      }
     }
   },
   'ProfilePic.update': function (selector, modifier) {
     if (this.userId) {
-      ProfilePic.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.ProfilePic.update)) {
+        ProfilePic.update(selector, modifier);
+      } else {
+        console.log('ProfilePic.update: access denied.');
+      }
     }
   }
 });
@@ -106,12 +170,20 @@ Meteor.methods({
 Meteor.methods({
   'Attachments.remove': function (id) {
     if (this.userId) {
-      Attachments.remove(id);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Attachments.remove)) {
+        Attachments.remove(id);
+      } else {
+        console.log('Attachments.remove: access denied.');
+      }
     }
   },
   'Attachments.update': function (selector, modifier) {
     if (this.userId) {
-      Attachments.update(selector, modifier);
+      if (Roles.userIsInRole(this.userId, Meteor.settings.allow.Attachments.update)) {
+        Attachments.update(selector, modifier);
+      } else {
+        console.log('Attachments.update: access denied.');
+      }
     }
   }
 });
