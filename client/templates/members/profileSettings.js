b76ec5ede8b495dfa083cc5f5c64be2b73370020
eugenkeller <eugenkeller.com@gmail.com>
2016-05-15 11:52:35 +0200

    Format client/templates/members/profileSettings.js

--- client/templates/members/profileSettings.js
+++ client/templates/members/profileSettings.js
@@ -4,13 +4,13 @@ pictureUrl = new ReactiveVar(false);
 
 settingsTab = new ReactiveVar("personal");
 
-Template.profileSettings.onRendered(function() {
+Template.profileSettings.onRendered(function () {
   // if scrolling is necessary $("html, body").animate({ scrollTop: 0 });
   window.scrollTo(0, 0);
   $('[data-toggle="tooltip"]').tooltip();
-  if(Session.get('signUpPicID')){
-  Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': Session.get('signUpPicID')}});
-  delete Session.keys['signUpPicID']
+  if (Session.get('signUpPicID')) {
+    Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': Session.get('signUpPicID')}});
+    delete Session.keys['signUpPicID']
   }
 });
 
@@ -18,12 +18,12 @@ Template.personal.helpers({
   userData: function () {
     return Meteor.user();
   },
-  profilePic: function() {
+  profilePic: function () {
     if (Meteor.user().profile.picture) {
       return true;
     }
   },
-  pictureID: function() {
+  pictureID: function () {
     if (Meteor.user().profile.pictureID) {
       return true;
     }
@@ -37,17 +37,18 @@ Template.social.helpers({
 });
 
 Template.profileSettings.helpers({
-  subscribed: function(){
-	  if(subscribers.find({email: Meteor.user().profile.email}).count() >0){
-		  return true;
-	  }else{
-		  return false;
-	  }
+  subscribed: function () {
+    if (subscribers.find({email: Meteor.user().profile.email}).count() > 0) {
+      return true;
+    } else {
+      return false;
+    }
   },
   setPersonal: function () {
     if (settingsTab.get() === "personal") {
       return true;
-    };
+    }
+    ;
   }
 });
 
@@ -62,7 +63,7 @@ Template.profileSettings.events({
     $('#js-seeSocial').addClass("active");
     $('#js-seePersonal').removeClass("active");
   },
-  'change .js-Profile': function(e, temp) {
+  'change .js-Profile': function (e, temp) {
     var image = new FS.File(e.target.files[0]);
     // Insert the image into the database
     // getting the image ID for use in the course object
@@ -73,42 +74,42 @@ Template.profileSettings.events({
 
     // The image id is stored in the image object
     var imageId = imageObject._id;
-	Session.set('imageId',imageId);
+    Session.set('imageId', imageId);
     // The url of the image
-    var imageUrl = Meteor.absoluteUrl() +"cfs/files/profilePic/"+imageId;
-	//Image uploaded token
-	Session.set('changes',true);
+    var imageUrl = Meteor.absoluteUrl() + "cfs/files/profilePic/" + imageId;
+    //Image uploaded token
+    Session.set('changes', true);
 
     //Store the old picture ID in a session variable, if the changes are discard the profilePic return to the original.
-	Session.set('oldPictureID', Meteor.user().profile.pictureID);
-	//Store the new picture ID in a session variable
-	Session.set('newPictureID', imageUrl);
-	//If the user upload a picture for the first time
-	if(!Meteor.user().profile.pictureID){
-	//The image appears
-	$('#hiddenImage').show();
-	}
+    Session.set('oldPictureID', Meteor.user().profile.pictureID);
+    //Store the new picture ID in a session variable
+    Session.set('newPictureID', imageUrl);
+    //If the user upload a picture for the first time
+    if (!Meteor.user().profile.pictureID) {
+      //The image appears
+      $('#hiddenImage').show();
+    }
     //Uploading the picture to S3 bucket.
-    $('.image').attr("src", Meteor.absoluteUrl()+"img/loading_bar.png")
-	$('.image').height('150px');
-	$('.image').width('150px');
-	$('.image').show();
-	//The file input manager disappears
-	$('#uploadInputFile').hide();
+    $('.image').attr("src", Meteor.absoluteUrl() + "img/loading_bar.png")
+    $('.image').height('150px');
+    $('.image').width('150px');
+    $('.image').show();
+    //The file input manager disappears
+    $('#uploadInputFile').hide();
 
     //Cheking if the picture are ready on the S3 bucket.
-	var checker = setInterval(function(){
-		$.get(Session.get('newPictureID')).done(function () {
-		 $('.image').attr("src", Session.get('newPictureID'));
-		 $('#uploadInput').css({position: 'absolute', marginLeft: "70x"});
-		 $('#uploadInput').show();
-		 $('#discardButton').css({position: 'absolute', marginLeft: "112px"});
-		 $('#discardButton').show();
-		 clearInterval(checker);
-         }).fail(function () {
-        })
-	},4500);
-	// assign the reactive var to the imageId
+    var checker = setInterval(function () {
+      $.get(Session.get('newPictureID')).done(function () {
+        $('.image').attr("src", Session.get('newPictureID'));
+        $('#uploadInput').css({position: 'absolute', marginLeft: "70x"});
+        $('#uploadInput').show();
+        $('#discardButton').css({position: 'absolute', marginLeft: "112px"});
+        $('#discardButton').show();
+        clearInterval(checker);
+      }).fail(function () {
+      })
+    }, 4500);
+    // assign the reactive var to the imageId
     if (imageId) {
       profilePicEdit = new ReactiveVar(imageId);
     }
@@ -118,93 +119,95 @@ Template.profileSettings.events({
     }
 
   },
-  'click #discard': function(){
-   //changes
-   Session.set('changes',true);
-   //If the user don't want to keep changes in profile picture, the pic returns to original, this is necessary to give a review of the new pic.
-   //Reset the preview to the old one and remove the picture from collection.
-    if(Session.get('oldPictureID')){
-	$('.image').attr("src", Session.get('oldPictureID'));
-	delete Session.keys['oldPictureID'];
-	}else{
-	//The image disappears
-	$('.image').hide();
-	//The discard button disappears
-	$('#discardButton').hide();
-    //The edit input button disappears
-	$('#uploadInput').hide();
-	//The file input manager appears
-	$('#uploadInputFile').show();
-	Session.set('newPictureID', false);
-	}
+  'click #discard': function () {
+    //changes
+    Session.set('changes', true);
+    //If the user don't want to keep changes in profile picture, the pic returns to original, this is necessary to give a review of the new pic.
+    //Reset the preview to the old one and remove the picture from collection.
+    if (Session.get('oldPictureID')) {
+      $('.image').attr("src", Session.get('oldPictureID'));
+      delete Session.keys['oldPictureID'];
+    } else {
+      //The image disappears
+      $('.image').hide();
+      //The discard button disappears
+      $('#discardButton').hide();
+      //The edit input button disappears
+      $('#uploadInput').hide();
+      //The file input manager appears
+      $('#uploadInputFile').show();
+      Session.set('newPictureID', false);
+    }
 
-	if(!Meteor.user().profile.pictureID){
-	//The image disappears
-	$('#hiddenImage').hide();
-	}
-	ProfilePic.remove({_id: Session.get('imageId')});
-   //Delete the Session variables.
+    if (!Meteor.user().profile.pictureID) {
+      //The image disappears
+      $('#hiddenImage').hide();
+    }
+    ProfilePic.remove({_id: Session.get('imageId')});
+    //Delete the Session variables.
     delete Session.keys['imageId'];
   },
-  'submit form': function (evt){
+  'submit form': function (evt) {
     evt.preventDefault();
     var temp = {};
 
     temp.firstName = $('#firstname').val();
     // temp.profile.lastname = $('#lastname').val();
-	currentEmail = Meteor.user().profile.email;
+    currentEmail = Meteor.user().profile.email;
     // console.log("done");
     // if changes
-	if(Session.get('changes')){
-    //Update the profile pic ID if the user changed the picture
-	  if(Session.get('newPictureID')){
-	  if(Session.get('imageId')){
-	  	Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': Session.get('newPictureID')}});
-	  }else{
-	  	Meteor.users.update({_id: Meteor.userId()}, {$set: {
-        'profile.pictureID': Session.get('oldPictureID'),
-        'profile.firstName': $('#firstname').val()
-      }
-      });
-	  }
+    if (Session.get('changes')) {
+      //Update the profile pic ID if the user changed the picture
+      if (Session.get('newPictureID')) {
+        if (Session.get('imageId')) {
+          Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': Session.get('newPictureID')}});
+        } else {
+          Meteor.users.update({_id: Meteor.userId()}, {
+            $set: {
+              'profile.pictureID': Session.get('oldPictureID'),
+              'profile.firstName': $('#firstname').val()
+            }
+          });
+        }
 
-	//Delete the Session variables.
-      delete Session.keys['imageId'];
-      delete Session.keys['newPictureID'];
-	  delete Session.keys['oldPictureID'];
-	  delete Session.keys['changes'];
-	  }else{
-	  Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': false}});
-	//Delete the Session variables.
-      delete Session.keys['imageId'];
-      delete Session.keys['newPictureID'];
-	  delete Session.keys['oldPictureID'];
-	  delete Session.keys['changes'];
-	  }
-	}
-	//subscribers collection updates if the email changed.
-	if (subscribers.find({email: currentEmail}).count() > 0
+        //Delete the Session variables.
+        delete Session.keys['imageId'];
+        delete Session.keys['newPictureID'];
+        delete Session.keys['oldPictureID'];
+        delete Session.keys['changes'];
+      } else {
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.pictureID': false}});
+        //Delete the Session variables.
+        delete Session.keys['imageId'];
+        delete Session.keys['newPictureID'];
+        delete Session.keys['oldPictureID'];
+        delete Session.keys['changes'];
+      }
+    }
+    //subscribers collection updates if the email changed.
+    if (subscribers.find({email: currentEmail}).count() > 0
       && subscribers.findOne({email: currentEmail}).email != $('#email').val()) {
       subscribers.insert({email: $('#email').val()});
-	  subscribers.remove({_id: subscribers.findOne({email: currentEmail})._id})
+      subscribers.remove({_id: subscribers.findOne({email: currentEmail})._id})
     }
 	
     // merge with above update to mongoDB
-    Meteor.users.update({_id: Meteor.userId()}, {$set: {
-      // from personal settings
-      "profile.firstName" : $('#firstname').val(),
-      "profile.lastName" : $('#lastname').val(),
-      "profile.institution" : $('#institution').val(),
-      "profile.interests" : $('#interests').val(),
-      "profile.position" : $('#position').val(),
-      "profile.email" : $('#email').val(),
+    Meteor.users.update({_id: Meteor.userId()}, {
+      $set: {
+        // from personal settings
+        "profile.firstName": $('#firstname').val(),
+        "profile.lastName": $('#lastname').val(),
+        "profile.institution": $('#institution').val(),
+        "profile.interests": $('#interests').val(),
+        "profile.position": $('#position').val(),
+        "profile.email": $('#email').val(),
 
-      //from social settings
-      "profile.website" : $('#website').val(),
-      "profile.blog" : $('#blog').val(),
-      "profile.twitter" : $('#twitter').val(),
-      "profile.linkedin" : $('#linkedin').val(),
-      "profile.facebook" : $('#facebook').val()
+        //from social settings
+        "profile.website": $('#website').val(),
+        "profile.blog": $('#blog').val(),
+        "profile.twitter": $('#twitter').val(),
+        "profile.linkedin": $('#linkedin').val(),
+        "profile.facebook": $('#facebook').val()
       }
     });
 
@@ -218,12 +221,12 @@ Template.profileSettings.events({
     Bert.alert("Changes saved.");
     // Meteor.users.update({_id: this._id}, {$set: temp} );
   },
-  'click #unsubscribe': function(){
-	 //Remove email from the subscribers collection
-	 subscribers.remove({_id: subscribers.findOne({email: Meteor.user().profile.email})._id})
+  'click #unsubscribe': function () {
+    //Remove email from the subscribers collection
+    subscribers.remove({_id: subscribers.findOne({email: Meteor.user().profile.email})._id})
   },
-  'click #subscribe': function(){
-	 //Insert email to the subscribers collection
-	 subscribers.insert({email: Meteor.user().profile.email});
+  'click #subscribe': function () {
+    //Insert email to the subscribers collection
+    subscribers.insert({email: Meteor.user().profile.email});
   }
 });
