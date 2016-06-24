c6e27640fc6381c3689ee52360c00ec5a592c146
Rogerio J <setaware@gmail.com>
2016-06-24 14:03:07 -0430

    router go

--- client/templates/admin/admin-members.js
+++ client/templates/admin/admin-members.js
@@ -1,5 +1,8 @@
 Template.adminMembersList.helpers({
   members: function () {
     return Meteor.users.find({}, {sort: {"profile.lastName": 1}});
+  },
+  url: function() {
+	  return Meteor.absoluteUrl();
   }
 });
