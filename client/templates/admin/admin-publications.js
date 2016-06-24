086cfa5ce1824782880724d51fff44e600411efc
Rogerio J <setaware@gmail.com>
2016-06-24 15:45:42 -0430

    Template refactor to purge authors / editors while creating a publication.

--- client/templates/admin/admin-publications.js
+++ client/templates/admin/admin-publications.js
@@ -122,75 +122,102 @@ Template.adminPublications.events({
     if (attachmentIdVar.get()) {
       temp.attachmentId = attachmentIdVar.get();
     }
+	//Authors generator
+    newAuthors = [];
+        authorFN = [];
+	    //Create empty array for the last name.
+	    authorLN = [];
+	    //Lenght of the css class author-first-n.
+	    numberOfAuthors =$('.author-first-n').length;
+	    //function that construct the updatedAuthors array
+	    for (i = 0; i < numberOfAuthors; i++) {
+            var author_firstName = $('.author-first-n')[i]; //Get the input info for the DOM
+			var author_lastName = $('.author-last-n')[i]    //Get the input info for the DOM
+			    authorFN[i] =author_firstName.value;        //Get the value of the input, prefilled, modified or created
+				authorLN[i] =author_lastName.value; 				//Get the value of the input, prefilled, modified or created
+				newAuthors[i] = {"lastName" : authorLN[i], //.lastName
+				                     "firstName" : authorFN[i], //.firstName
+                                     "fullName" : authorFN[i] + ' ' + authorLN[i] //Full name
+									 };
+        }
+	//Purgue the array of empty fields
+	    newAuthorsFixed = [];
+	    for (i = 0; i < newAuthors.length; i++) {
+                if(newAuthors[i].fullName != ' '){
+					newAuthorsFixed[newAuthorsFixed.length] = newAuthors[i];
+				}
+        }
+		//Check the authors to add
+        console.log('Authors');
+		for (i = 0; i < newAuthorsFixed.length; i++) {
+               console.log(newAuthorsFixed[i].fullName)
+        }; 
+    //Editors generator
+    newEditors = [];
+	//Create empty array for the first name.
+	editorFN = [];
+	//Create empty array for the last name.
+	editorLN = [];
+	//Lenght of the css class editor-first-n.
+	numberOfEditors =$('.editors-first-n').length;
+	//function that construct the numberOfEditors array
+	    for (i = 0; i < numberOfEditors; i++) {
+            var editor_firstName = $('.editors-first-n')[i]; //Get the input info for the DOM
+			var editor_lastName = $('.editors-last-n')[i]    //Get the input info for the DOM
+			    editorFN[i] =editor_firstName.value;        //Get the value of the input, prefilled, modified or created
+				editorLN[i] =editor_lastName.value;         //Get the value of the input, prefilled, modified or created
+				newEditors[i] = {"lastName" : editorLN[i], //.lastName
+				                     "firstName" : editorFN[i], //.firstName
+                                     "fullName" : editorFN[i] + ' ' + editorLN[i]
+									 };
+        }
+		//Purgue the array of empty fields
+		newEditorsFixed = [];
+	    for (i = 0; i < newEditors.length; i++) {
+                if(newEditors[i].fullName != ' '){
+					newEditorsFixed[newEditorsFixed.length] = newEditors[i];
+				}
+        }
+		//Check the editors to add
+        console.log('editors');
+		for (i = 0; i < newEditorsFixed.length; i++) {
+               console.log(newEditorsFixed[i].fullName)
+        }; 
+    //Create the news
+	news = {};
+	news.title = "A New Publication Added to Bibliography";
+	news.description = "The publication \"" + $('#title').val() + "\" was added to the bibliography. ";
+	news.type = "news";
+	news.createdAt = moment().format('ddd, DD MMM YYYY hh:mm:ss');
+	news.publishedAt = moment().format('ddd, DD MMM YYYY hh:mm:ss');
+	news.publishedRawFormat = new Date();
+ 	news.keywords = ["publication"];
+	//Update the news
+        Meteor.call('NewsEvents.insert', {news});
+    //Create the publication
+    pub = {};
+    pub.title = $('#title').val();
+    pub.authors = newAuthorsFixed;
+    pub.editors = newEditorsFixed;
+    pub.year = $('#year').val();
+    pub.type = $('input[name=outlet-type]:checked').val();
+    pub.link = $('#link').val();
+    pub.abstract = $('#abstract').summernote('code');
+    pub.cleanAbstract = $('#abstract').summernote('code').replace(/<\/?[^>]+(>|$)/g, "");
+    pub.outlet = selectedOutlet;
 
-		var newAuthors = [];
-    // make a field full name field for the easysearch package
-    var newAuthorsFullName = [];
-		inputsAuthors = Session.get('inputsAuthors');
-		_.each(inputsAuthors, function(input) {
-      newAuthors.push({firstName: $('#' + input.uniqidFirst).val(),
-                      lastName: $('#' + input.uniqidLast).val()},
-                      {fullName: $('#' + input.uniqidFirst).val() + ' ' + $('#' + input.uniqidLast).val()});
-      //newAuthorsFullName.push($('#' + input.uniqidFirst).val() + ' ' + $('#' + input.uniqidLast).val());
-		});
+    // Published Paper (pp)
+    pub.journal = $('#journal').val();
+    // Published Paper (pp) or Book Chapter (bc)
+    pub.pages = $('#pages').val();
 
-		var newEditors = [];
-    // make a field full name field for the easysearch package
-    var newEditorsFullName = [];
-		inputsEditors = Session.get('inputsEditors');
-		_.each(inputsEditors, function(input) {
-		  newEditors.push({firstName: $('#' + input.uniqidFirst).val(),
-                      lastName: $('#' + input.uniqidLast).val()},
-                      {fullName: $('#' + input.uniqidFirst).val() + ' ' + $('#' + input.uniqidLast).val()});
-      newEditorsFullName.push($('#' + input.uniqidFirst).val() + ' ' + $('#' + input.uniqidLast).val());
-		});
-
-		// add the first (i.e, default) author to the array
-		newAuthors.unshift({firstName: $('#firstAuName').val(), lastName: $('#lastAuName').val()},
-                {fullName: $('#firstAuName').val() + ' ' + $('#lastAuName').val()});
-    newAuthorsFullName.unshift($('#firstAuName').val() + ' ' + $('#lastAuName').val());
-
-		newEditors.unshift({firstName: $('#firstEdName').val(), lastName: $('#lastEdName').val()},
-                {fullName: $('#firstEdName').val() + ' ' + $('#lastEdName').val()});
-    newEditorsFullName.unshift($('#firstEdName').val() + ' ' + $('#lastEdName').val());
-
-    Meteor.call('NewsEvents.insert', {
-      title: "A New Publication Added to Bibliography",
-      description: "The publication \"" + $('#title').val() + "\" was added to the bibliography. ",
-      type: "news",
-      createdAt: moment().format('ddd, DD MMM YYYY hh:mm:ss'),
-      publishedAt: moment().format('ddd, DD MMM YYYY hh:mm:ss'),
-      publishedRawFormat: new Date(),
-      keywords: ["publication"]
-    });
-
-		Meteor.call('Publications.insert', {
-			title: $('#title').val(),
-			//authors: [$('#authors').val()],
-      fullNameAuthors: newAuthorsFullName,
-			authors: newAuthors,
-      fullNameEditors: newEditorsFullName,
-			editors: newEditors,
-			year: $('#year').val(),
-      link: $('#link').val(),
-			type: $('input[name=outlet-type]:checked').val(),
-			abstract: $('#abstract').summernote('code'),
-      cleanAbstract: $('#abstract').summernote('code').replace(/<\/?[^>]+(>|$)/g, ""),
-      outlet: selectedOutlet,
-
-			// Published Paper (pp)
-			journal: $('#journal').val(),
-			// Published Paper (pp) or Book Chapter (bc)
-			pages: $('#pages').val(),
-
-			// Book (bk) or Book Chapter (bc)
-			publisher: $('#publisher').val(),
-			location: $('#location').val(),
-
-      attachmentId: temp.attachmentId,
-			createdAt: new Date ()
-		});
-        //Fire the email to all subscriptors
+    // Book (bk) or Book Chapter (bc)
+    pub.publisher = $('#publisher').val();
+    pub.location = $('#location').val();
+	//Publication insert
+		Meteor.call('Publications.insert', {pub});
+		
+    //Fire the email to all subscriptors
 		var publication = '<header><img src="http://openstrategynetwork.com/img/osn_logoneu.png"></header><body style="background:#0B676E;color:#FFFFFF"><center>' +'<h1>New publication uploaded to our platform!</h1>'+ '<h2>Title: '+$('#title').val()+'</h2>' + '<h2>Abstract:</h2>'+'<h3>'+$('#abstract').summernote('code')+'</h3></center></body>';
             for (i = 0; i < Subscribers.find().count(); i++) {
             var email_ = Subscribers.find().fetch()[i].email;
@@ -200,16 +227,10 @@ Template.adminPublications.events({
                             'New publication in Open Strategy Network', //subject
                              publication+'<footer style="background:#CCCCCC;color:black;"><center><h4>To unsubscribe to our notifications go to <a href='+Meteor.absoluteUrl()+'unsubscribe?='+email_+'>http://openstrategynetwork.com/unsubscribe</a></h4><center></footer>');
             }
-
+    //Reset the form
 		$('#addPub')[0].reset();
 		$('#abstract').summernote('code', '');
-
-		// $('#title').val('');
-		// $('#authors').val('');
-		// $('#year').val('');
-		// $('#abstract').val('');
-		// $("input:radio").removeAttr("checked");
-
+    //Bert alert
 		Bert.alert("Publication added to bibliography.");
 	}
 });
