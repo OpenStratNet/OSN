8bdba1011544028776e3e5c00be43d47a369c810
Amir Rahbaran <amirrahbaran@hotmail.com>
2016-07-18 12:20:50 +0200

    when deleting member: also delete member from subscriber list when

--- client/helpers/deleteHelpers.js
+++ client/helpers/deleteHelpers.js
@@ -26,6 +26,12 @@ Template.applicationLayout.events({
   'click .jsMembersDelUser': function (evt, temp) {
     var deleteConfirmation = confirm('Really delete this member?');
     if (deleteConfirmation) {
+      var toBeDeletedUser = Meteor.users.findOne({_id: this._id});
+      var currentEmail = toBeDeletedUser.profile.email;
+      console.log(currentEmail);
+      if (Subscribers.find({email: currentEmail}).count() > 0 ) {
+        Meteor.call('Subscribers.remove', {_id: Subscribers.findOne({email: currentEmail})._id});
+      }
       Meteor.users.remove({_id: this._id});
     }
   }
